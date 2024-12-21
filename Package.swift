ad4478b24326db8abb850f0bb170ab7be159b58f
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-21 17:59:08 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -12,17 +12,9 @@ extension Target.Dependency {
 }
 
 extension Target.Dependency {
+    static var coenttbWeb: Self { .product(name: "CoenttbWeb", package: "coenttb-web") }
     static var basicAuth: Self { .product(name: "BasicAuth", package: "swift-authentication") }
-    static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
-    static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
     static var dependenciesTestHelpers: Self { .product(name: "DependenciesTestSupport", package: "swift-dependencies") }
-    static var logging: Self { .product(name: "Logging", package: "swift-log") }
-    static var memberwiseInit: Self { .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro") }
-    static var tagged: Self { .product(name: "Tagged", package: "swift-tagged") }
-    static var urlRouting: Self { .product(name: "URLRouting", package: "swift-url-routing") }
-    static var urlFormCoding: Self { .product(name: "UrlFormCoding", package: "swift-web") }
-    static var swiftDate: Self { .product(name: "Date", package: "swift-date") }
-    static var urlFormEncoding: Self { .product(name: "UrlFormEncoding", package: "swift-web") }
     static var environmentVariables: Self { .product(name: "EnvironmentVariables", package: "swift-environment-variables") }
     static var issueReporting: Self { .product(name: "IssueReporting", package: "xctest-dynamic-overlay") }
 }
@@ -37,31 +29,19 @@ let package = Package(
         .library(name: .mailgun, targets: [.mailgun]),
     ],
     dependencies: [
-        .package(url: "https://github.com/coenttb/swift-web", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-date", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-authentication.git", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-environment-variables.git", branch: "main"),
-        .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro", from: "0.3.0"),
+        .package(url: "https://github.com/coenttb/coenttb-web", branch: "main"),
+        .package(url: "https://github.com/coenttb/swift-environment-variables", branch: "main"),
+        .package(url: "https://github.com/coenttb/swift-authentication", branch: "main"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.1.5"),
-        .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.10.0"),
-        .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.0"),
-        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay.git", from: "1.4.3"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.3"),
     ],
     targets: [
         .target(
             name: .mailgun,
             dependencies: [
                 .basicAuth,
-                .dependencies,
-                .dependenciesMacros,
-                .memberwiseInit,
-                .tagged,
-                .swiftDate,
-                .urlFormCoding,
-                .environmentVariables,
-                .issueReporting,
-                .urlRouting
-                
+                .coenttbWeb,
+                .issueReporting
             ]
         ),
         .testTarget(
